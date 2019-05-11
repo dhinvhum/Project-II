9b3c13f39a3a3361a8e0b223baec0435d61a747f
dhinvhum <dhinbhum@gmail.com>
2019-05-12 05:19:31 +0700

    update validation

--- dashboard.js
+++ dashboard.js
@@ -1,33 +1,33 @@
-$(function() {
-    $.get("data/products.json", function(data) {
-        var i =0
-        for(i=0;i<=data.length;i++){
-         var x=0;
-         x += i;
+$(function () {
+    $.get("data/products.json", function (data) {
+        var i = 0
+        for (i = 0; i <= data.length; i++) {
+            var x = 0;
+            x += i;
         }
         $("#products").append(x);
     });
-    $.get("data/customers.json", function(data) {
-        var i =0
-        for(i=0;i<=data.length;i++){
-         var x=0;
-         x += i;
+    $.get("data/customers.json", function (data) {
+        var i = 0
+        for (i = 0; i <= data.length; i++) {
+            var x = 0;
+            x += i;
         }
         $("#customers").append(x);
     });
-    $.get("data/suppliers.json", function(data) {
-        var i =0
-        for(i=0;i<=data.length;i++){
-         var x=0;
-         x += i;
+    $.get("data/suppliers.json", function (data) {
+        var i = 0
+        for (i = 0; i <= data.length; i++) {
+            var x = 0;
+            x += i;
         }
         $("#suppliers").append(x);
     });
-    $.get("data/orders.json", function(data) {
-        var i =0
-        for(i=0;i<=data.length;i++){
-         var x=0;
-         x += i;
+    $.get("data/orders.json", function (data) {
+        var i = 0
+        for (i = 0; i <= data.length; i++) {
+            var x = 0;
+            x += i;
         }
         $("#orders").append(x);
     });
